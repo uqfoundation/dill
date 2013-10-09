90037c8d3a8b0077eef9c0bf8f25539527a67fc4
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2013-10-09 03:24:49 +0000

    moved object creation out of dill_test2, into 'objects' as dicts; added objtypes which reads objects dicts and generates type for given objects TODO: renaming of objects and objtypes -- move out of tests and into dill
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/dill@519 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- tests/dill_test2.py
+++ tests/dill_test2.py
@@ -1,229 +1,53 @@
 #!/usr/bin/env python
 """
 demonstrate dill's ability to pickle different python types
+test pickling of all Python Standard Library objects (currently: CH 1-14 @ 2.7)
 """
-# pickle-test all Python Standard Library objects (currently: CH 1-14 @ 2.7)
 
 import dill as pickle
-#pickle._trace(True)
+#pickle.debug.trace(True)
 #import pickle
 
-typelist = []
-import warnings; warnings.filterwarnings("ignore", category=DeprecationWarning)
+# get all objects for testing
+from objects import succeeds as test_objects
+from objects import failures
+#test_objects.update(failures)
 
-# testing types
-_none = None; typelist.append(_none)
-_type = type; typelist.append(_type)
-_bool = bool(1); typelist.append(_bool)
-_int = int(1); typelist.append(_int)
-_long = long(1); typelist.append(_long)
-_float = float(1); typelist.append(_float)
-_complex = complex(1); typelist.append(_complex)
-_string = str(1); typelist.append(_string)
-_unicode = unicode(1); typelist.append(_unicode)
-_tuple = (); typelist.append(_tuple)
-_list = []; typelist.append(_list)
-_dict = {}; typelist.append(_dict)
-_file = file; typelist.append(_file)
-_buffer = buffer; typelist.append(_buffer)
-_builtin = len; typelist.append(_builtin)
+# helper objects
 class _class:
     def _method(self):
         pass
-class _newclass(object):
-    def _method(self):
-        pass
-typelist.append(_class)
-typelist.append(_newclass) # <type 'type'>
-_instance = _class(); typelist.append(_instance)
-_object = _newclass(); typelist.append(_object) # <type 'class'>
-_object2 = object(); typelist.append(_object2)
-_set = set(); typelist.append(_set)
-_frozenset = frozenset(); typelist.append(_frozenset)
-import sets; _set2 = sets.Set(); typelist.append(_set2)  # Deprecated
-_immutableset = sets.ImmutableSet(); typelist.append(_immutableset)
-import array; _array = array.array("f"); typelist.append(_array)
-def _function2():
-    try: raise
-    except:
-      from sys import exc_info
-      e, er, tb = exc_info()
-      return er, tb
-_exception = _function2()[0]; typelist.append(_exception)
-import decimal; _decimal = decimal.Decimal(1); typelist.append(_decimal)
-_copyright = copyright; typelist.append(_copyright)
-def _function(x): yield x; typelist.append(_function)
-import logging; _logger = logging.getLogger(); typelist.append(_logger)
-import re; _srepattern = re.compile(''); typelist.append(_srepattern)
-import collections; _deque = collections.deque([0]); typelist.append(_deque)
-_ddict = collections.defaultdict(_function, _dict); typelist.append(_ddict)
-import datetime; _tzinfo = datetime.tzinfo(); typelist.append(_tzinfo)
-_datetime = datetime.datetime.today(); typelist.append(_datetime)
-import calendar; _calendar = calendar.Calendar(); typelist.append(_calendar)
-import mutex; _mutex = mutex.mutex(); typelist.append(_mutex)
-import itertools; _count = itertools.count(0); typelist.append(_count)
-import tarfile; _tinfo = tarfile.TarInfo(); typelist.append(_tinfo)
-try: # python 2.6
-  import fractions; _fraction = fractions.Fraction(); typelist.append(_fraction)
-  import number; _number = numbers.Number(); typelist.append(_number)
-  _bytearray = bytearray([1]); typelist.append(_bytearray)
-except ImportError:
-  pass
-try: # python 2.7
-  _odict = collections.OrderedDict(_dict); typelist.append(_odict) # 2.7
-  _counter = collections.Counter(_dict); typelist.append(_counter) # 2.7
-except AttributeError:
-  pass
-# pickle fails on all below here -------------------------------------------
-_lambda = lambda x: lambda y: x; typelist.append(_lambda)
-_cell = (_lambda)(0).func_closure[0]; typelist.append(_cell)
-_method = _class()._method; typelist.append(_method)
-_ubmethod = _class._method; typelist.append(_ubmethod)
-_module = pickle; typelist.append(_module)
-_code = compile('','','exec'); typelist.append(_code)
-_dictproxy = type.__dict__; typelist.append(_dictproxy)
-_dictprox2 = _newclass.__dict__; typelist.append(_dictprox2)
-_methoddescr = type.__dict__['mro']; typelist.append(_methoddescr)
-_memdescr = datetime.timedelta.days; typelist.append(_memdescr)
-_memdescr2 = type.__dict__['__weakrefoffset__']; typelist.append(_memdescr2)
-import array; _getsetdescr = array.array.typecode; typelist.append(_getsetdescr)
-_wrapperdescr = type.__repr__; typelist.append(_wrapperdescr)
-_wrapperdescr2 = type.__dict__['__module__']; typelist.append(_wrapperdescr2)
-_xrange = xrange(1); typelist.append(_xrange)
-_slice = slice(1); typelist.append(_slice)
-_nimp = NotImplemented; typelist.append(_nimp)
-_ellipsis = Ellipsis; typelist.append(_ellipsis)
-_staticmethod = staticmethod(_method); typelist.append(_staticmethod)
-_classmethod = classmethod(_method); typelist.append(_classmethod)
-_property = property(); typelist.append(_property)
-_super = super(type); typelist.append(_super)
-import functools; _part = functools.partial(int,base=2); typelist.append(_part)
-_izip = itertools.izip('0','1'); typelist.append(_izip)
-_chain = itertools.chain('0','1'); typelist.append(_chain)
-import threading; _lock = threading.Lock(); typelist.append(_lock)
-_rlock = threading.RLock(); typelist.append(_rlock)
-import Queue; _queue = Queue.Queue(); typelist.append(_queue)
-import sqlite3; _conn = sqlite3.connect(':memory:'); typelist.append(_conn)
-_cursor = _conn.cursor(); typelist.append(_cursor)
-import tempfile; _file2 = tempfile.TemporaryFile('w'); typelist.append(_file2)
-_filedescrip, _tempfile = tempfile.mkstemp('w') # used for pickle-testing
-import bz2; _bz2 = bz2.BZ2File(_tempfile); typelist.append(_bz2)
-_bz2compress = bz2.BZ2Compressor(); typelist.append(_bz2compress)
-_bz2decompress = bz2.BZ2Decompressor(); typelist.append(_bz2decompress)
-#import zipfile; _zip = zipfile.ZipFile(_tempfile,'w'); typelist.append(_zip)
-#_zip.write(_tempfile,'x'); _zinfo = _zip.getinfo('x'); typelist.append(_zinfo)
-_tar = tarfile.open(fileobj=_file2,mode='w'); typelist.append(_tar)
-import csv; _dialect = csv.get_dialect('excel'); typelist.append(_dialect)
-import pprint; _printer = pprint.PrettyPrinter(); typelist.append(_printer)
-import socket; _socket = socket.socket(); typelist.append(_socket)
-import contextlib; _ctxmgr = contextlib.GeneratorContextManager(max); typelist.append(_ctxmgr)
-try:
-  __IPYTHON__ is True # is ipython
-except NameError:
-  _quitter = quit; typelist.append(_quitter)
-try:
-  from numpy import ufunc as _numpy_ufunc
-  typelist.append(_numpy_ufunc)
-  from numpy import array as _numpy_array
-  typelist.append(_numpy_array)
-  from numpy import int32 as _numpy_int32
-  typelist.append(_numpy_int32)
-except ImportError:
-  pass
-try: # python 2.6
-  _product = itertools.product('0','1'); typelist.append(_product)
-except AttributeError:
-  pass
-# dill fails in 2.5/2.6 below here -------------------------------------------
-#import gzip; _gzip = gzip.GzipFile(fileobj=_file2); typelist.append(_gzip)
-# dill fails on all below here -------------------------------------------
-_traceback = _function2()[1]; typelist.append(_traceback)
-_generator = _function(1); typelist.append(_generator)
-_frame = _generator.gi_frame; typelist.append(_frame)
-#_socketpair = _socket._sock
-##_logger2 = logging.getLogger(__name__)
-##_methodwrapper = (1).__repr__
-##_listiter = iter(_list)
-##_tupleiter = iter(_tuple)
-##_xrangeiter = iter(_xrange)
-##_setiter = iter(_set)
-##_cycle = itertools.cycle('0')
-##_repeat = itertools.repeat(0) # 2.7
-##_compress = itertools.compress('0',[1]) # etc
-##_permute = itertools.permutations('0')
-##_combine = itertools.combinations('0',1)
-##_dictitemiter = type.__dict__.iteritems()
-##_dictkeyiter = type.__dict__.iterkeys()
-##_dictvaliter = type.__dict__.itervalues()
-##_dictitems = _dict.viewitems() # 2.7
-##_dictkeys = _dict.viewkeys() # 2.7
-##_dictvalues = _dict.viewvalues() # 2.7
-##_memory = memoryview('0') # 2.7
-##_memory2 = memoryview(bytearray('0')) # 2.7
-##import cStringIO; _stringi = cStringIO.InputType
-##_stringo = cStringIO.OutputType
-##import operator; _itemgetter = operator.itemgetter(0)
-##_attrgetter = operator.attrgetter('__repr__')
-##_methodcall = operator.methodcaller('mro') # 2.6
-##import struct; _struct = struct.Struct('c')
-import weakref; _ref = weakref.ref(_instance); typelist.append(_ref)
-##_deadref = weakref.ref(_class()); typelist.append(_deadref)
-#_proxy = weakref.proxy(_instance); typelist.append(_proxy)
-##_deadproxy = weakref.proxy(_class()); typelist.append(_deadproxy)
-class _class2:
-    def __call__(self):
-        pass
-_instance2 = _class2()
-#_callable = weakref.proxy(_instance2); typelist.append(_callable)
-##_deadcallable = weakref.proxy(_class2()); typelist.append(_deadcallable)
-##_weakset = weakref.WeakSet(); typelist.append(_weakset) # 2.7
-##_weakkeydict = weakref.WeakKeyDictionary(); typelist.append(_weakkeydict)
-##_weakvaldict = weakref.WeakValueDictionary(); typelist.append(_weakvaldict)
-##_callableiter = _srepattern.finditer(''); typelist.append(_callableiter)
-##_srematch = _srepattern.match(''); typelist.append(_srematch)
-##_srescanner = _srepattern.scanner(''); typelist.append(_srescanner)
-##import codecs; _streamreader = codecs.StreamReader(_file2) # etc
-##import locale; _cmpkey = functools.cmp_to_key(locale.strcoll) # 2.7
-##_cmpkeyobj = _cmpkey('0') #2.7
-##import shelve; _shelve = shelve.Shelf({})
-##import dbm; _dbm = dbm.open('foo','n')
-##import anydbm; _dbcursor = anydbm.open('foo','n')
-##_db = _dbcursor.db
-##import zlib; _zcompress = zlib.compressobj()
-##_zdecompress = zlib.decompressobj()
-##_csvreader = csv.reader(_file2)
-##_csvwriter = csv.writer(_file2)
-##_csvdreader = csv.DictReader(_file2)
-##_csvdwriter = csv.DictWriter(_file2,{})
-##import xdrlib; _xdr = xdrlib.Packer()
-##import hashlib; _hash = hashlib.md5()
-##import hmac; _hmac = hmac.new('')
-# cleanup -------------------------------------------
-import os; os.remove(_tempfile)
+# objects that *fail* if imported
+special = {}
+special['LambdaType'] = _lambda = lambda x: lambda y: x
+special['MethodType'] = _method = _class()._method
+special['UnboundMethodType'] = _class._method
+test_objects.update(special)
+
+def pickles(obj,exact=False):
+    """quick check if object pickles with dill"""
+    try:
+        pik = pickle.loads(pickle.dumps(obj))
+        if exact:
+            try:
+                assert pik == obj
+            except AssertionError, err:
+                assert type(obj) == type(pik)
+                print "weak: %s" % type(obj)
+        else:
+            assert type(obj) == type(pik)
+    except (TypeError, AssertionError, pickle.PicklingError, \
+                                       pickle.UnpicklingError), err:
+        print "COPY failure: %s" % type(obj)
+    return
 
 
 if __name__ == '__main__':
 
-  def pickles(x):
-    #print type(x)
-    try:
-      p = pickle.loads(pickle.dumps(x))
-      try:
-        assert x == p
-      except AssertionError, err:
-        assert type(x) == type(p)
-        print "weak: %s" % type(x)
-    except (TypeError, pickle.PicklingError), err:
-      print "COPY failure: %s" % type(x)
-    return
+    for member in test_objects.values():
+       #print "%s ==> %s" % (member, type(member)) # DEBUG
+       #pickles(member, exact=True)
+        pickles(member, exact=False)
 
-  for member in typelist:
-     #print "%s ==> %s" % (member, type(member)) # DEBUG
-      pickles(member)
-  for member in typelist:
-     #print "%s ==> %s" % (member, type(member)) # DEBUG
-      try:
-          pickle.loads(pickle.dumps(member))
-      except:
-          print "PICKLE failure: %s" % type(member)
 
+# EOF
---
+++ tests/objects.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+"""
+all Python Standard Library objects (currently: CH 1-14 @ 2.7)
+and some other common objects (i.e. numpy.ndarray)
+"""
+
+__all__ = ['registered','failures','succeeds']
+
+# helper imports
+import warnings; warnings.filterwarnings("ignore", category=DeprecationWarning)
+import sets # Deprecated
+import re
+import array
+import collections
+import cStringIO
+import codecs
+import struct
+import datetime
+import calendar
+import mutex
+import weakref
+import Queue
+import pprint
+import decimal
+import functools
+import itertools
+import operator
+import tempfile
+import sqlite3
+import bz2
+import gzip
+import zipfile
+import tarfile
+import csv
+import logging
+import threading
+import socket
+import contextlib
+
+# helper objects
+class _class:
+    def _method(self):
+        pass
+#   @classmethod
+#   def _clsmethod(cls): #XXX: test me
+#       pass
+#   @staticmethod
+#   def _static(self): #XXX: test me
+#       pass
+class _class2:
+    def __call__(self):
+        pass
+_instance2 = _class2()
+class _newclass(object):
+    def _method(self):
+        pass
+#   @classmethod
+#   def _clsmethod(cls): #XXX: test me
+#       pass
+#   @staticmethod
+#   def _static(self): #XXX: test me
+#       pass
+def _function(x): yield x
+def _function2():
+    try: raise
+    except:
+        from sys import exc_info
+        e, er, tb = exc_info()
+        return er, tb
+_filedescrip, _tempfile = tempfile.mkstemp('w') # deleted in cleanup
+
+# objects used by dill for type declaration
+registered = d = {}
+# objects dill fails to pickle
+failures = x = {}
+# all other type objects
+succeeds = a = {}
+
+# types module (part of CH 8)
+a['BooleanType'] = bool(1)
+a['BufferType'] = buffer
+a['BuiltinFunctionType'] = len
+a['BuiltinMethodType'] = a['BuiltinFunctionType']
+a['ClassType'] = _class
+a['ComplexType'] = complex(1)
+a['DictType'] = _dict = {}
+a['DictionaryType'] = a['DictType']
+a['FileType'] = file
+a['FloatType'] = float(1)
+a['FunctionType'] = _function
+a['InstanceType'] = _instance = _class()
+a['IntType'] = int(1)
+a['ListType'] = _list = []
+a['LongType'] = long(1)
+a['NoneType'] = None
+a['ObjectType'] = object()
+a['StringType'] = str(1)
+a['TupleType'] = _tuple = ()
+a['TypeType'] = type
+a['UnicodeType'] = unicode(1)
+# built-in constants (CH 4)
+a['CopyrightType'] = copyright
+# built-in types (CH 5)
+a['ClassObjectType'] = _newclass # <type 'type'>
+a['ClassInstanceType'] = _newclass() # <type 'class'>
+a['SetType'] = _set = set()
+a['FrozenSetType'] = frozenset()
+# built-in exceptions (CH 6)
+a['ExceptionType'] = _function2()[0]
+# string services (CH 7)
+a['RegexPatternType'] = _srepattern = re.compile('')
+# data types (CH 8)
+a['SetsType'] = sets.Set()
+a['ImmutableSetType'] = sets.ImmutableSet()
+a['ArrayType'] = array.array("f")
+a['DequeType'] = collections.deque([0])
+a['DefaultDictType'] = collections.defaultdict(_function, _dict)
+a['TZInfoType'] = datetime.tzinfo()
+a['DateTimeType'] = datetime.datetime.today()
+a['CalendarType'] = calendar.Calendar()
+a['MutexType'] = mutex.mutex()
+# numeric and mathematical types (CH 9)
+a['DecimalType'] = decimal.Decimal(1)
+a['CountType'] = itertools.count(0)
+# data compression and archiving (CH 12)
+a['TarInfoType'] = tarfile.TarInfo()
+# generic operating system services (CH 15)
+a['LoggerType'] = logging.getLogger()
+
+try: # python 2.6
+    import fractions
+    import number
+    # built-in functions (CH 2)
+    a['ByteArrayType'] = bytearray([1])
+    # numeric and mathematical types (CH 9)
+    a['FractionType'] = fractions.Fraction()
+    a['NumberType'] = numbers.Number()
+except ImportError:
+    pass
+try: # python 2.7
+    # data types (CH 8)
+    a['OrderedDictType'] = collections.OrderedDict(_dict)
+    a['CounterType'] = collections.Counter(_dict)
+except AttributeError:
+    pass
+
+# -- pickle fails on all below here -----------------------------------------
+# types module (part of CH 8)
+a['CodeType'] = compile('','','exec')
+a['DictProxyType'] = type.__dict__
+a['EllipsisType'] = Ellipsis
+a['GetSetDescriptorType'] = array.array.typecode
+x['LambdaType'] = _lambda = lambda x: lambda y: x #XXX: works when not imported!
+a['MemberDescriptorType'] = type.__dict__['__weakrefoffset__']
+a['MemberDescriptorType2'] = datetime.timedelta.days
+x['MethodType'] = _method = _class()._method #XXX: works when not imported!
+a['ModuleType'] = datetime
+a['NotImplementedType'] = NotImplemented
+a['SliceType'] = slice(1)
+x['UnboundMethodType'] = _class._method #XXX: works when not imported!
+a['XRangeType'] = _xrange = xrange(1)
+# other (concrete) object types
+a['CellType'] = (_lambda)(0).func_closure[0]
+a['MethodDescriptorType'] = type.__dict__['mro']
+a['WrapperDescriptorType'] = type.__repr__
+a['WrapperDescriptorType2'] = type.__dict__['__module__']
+# built-in functions (CH 2)
+a['StaticMethodType'] = staticmethod(_method)
+a['ClassMethodType'] = classmethod(_method)
+a['PropertyType'] = property()
+a['SuperType'] = super(type)
+# data types (CH 8)
+a['QueueType'] = Queue.Queue()
+a['PrettyPrinterType'] = pprint.PrettyPrinter()
+# numeric and mathematical types (CH 9)
+a['PartialType'] = functools.partial(int,base=2)
+a['IzipType'] = itertools.izip('0','1')
+a['ChainType'] = itertools.chain('0','1')
+# file and directory access (CH 10)
+a['TemporaryFileType'] = _file2 = tempfile.TemporaryFile('w')
+# data persistence (CH 11)
+a['ConnectionType'] = _conn = sqlite3.connect(':memory:')
+a['CursorType'] = _conn.cursor()
+# data compression and archiving (CH 12)
+a['BZ2FileType'] = bz2.BZ2File(_tempfile)
+a['BZ2CompressorType'] = bz2.BZ2Compressor()
+a['BZ2DecompressorType'] = bz2.BZ2Decompressor()
+# a['ZipFileType'] = _zip = zipfile.ZipFile(_tempfile,'w')
+# _zip.write(_tempfile,'x') # FIXME: pickling throws weird error
+# a['ZipInfoType'] = _zip.getinfo('x')
+a['TarFileType'] = tarfile.open(fileobj=_file2,mode='w')
+# file formats (CH 13)
+a['Dialect'] = csv.get_dialect('excel')
+# optional operating system services (CH 16)
+a['LockType'] = threading.Lock()
+a['RLockType'] = threading.RLock()
+# interprocess communication (CH 17)
+a['SocketType'] = _socket = socket.socket()
+# python runtime services (CH 27)
+a['GeneratorContextManagerType'] = contextlib.GeneratorContextManager(max)
+
+try: # ipython
+    __IPYTHON__ is True # is ipython
+except NameError:
+    # built-in constants (CH 4)
+    a['QuitterType'] = quit
+    a['ExitType'] = a['QuitterType']
+try: # numpy
+    from numpy import ufunc as _numpy_ufunc
+    from numpy import array as _numpy_array
+    from numpy import int32 as _numpy_int32
+    a['NumpyUfuncType'] = _numpy_ufunc
+    a['NumpyArrayType'] = _numpy_array
+    a['NumpyInt32Type'] = _numpy_int32
+except ImportError:
+    pass
+try: # python 2.6
+    # numeric and mathematical types (CH 9)
+    a['ProductType'] = itertools.product('0','1')
+except AttributeError:
+    pass
+
+# -- dill fails in 2.5/2.6 below here ---------------------------------------
+x['GzipFileType'] = gzip.GzipFile(fileobj=_file2)
+# -- dill fails on all below here -------------------------------------------
+# types module (part of CH 8)
+x['DictProxyType2'] = _newclass.__dict__
+x['GeneratorType'] = _generator = _function(1) #XXX: priority
+x['FrameType'] = _generator.gi_frame #XXX: inspect.currentframe()
+x['TracebackType'] = _function2()[1] #(see: inspect.getouterframes,getframeinfo)
+# other (concrete) object types
+# (also: Capsule, CObject, ...?)
+# built-in functions (CH 2)
+x['MethodWrapperType'] = [].__repr__ #XXX: priority
+x['ListIteratorType'] = iter(_list) #XXX: empty vs non-empty
+x['TupleIteratorType']= iter(_tuple) #XXX: empty vs non-empty
+x['XRangeIteratorType'] = iter(_xrange) #XXX: empty vs non-empty
+x['SetIteratorType'] = iter(_set) #XXX: empty vs non-empty
+# built-in types (CH 5)
+x['DictionaryItemIteratorType'] = type.__dict__.iteritems()
+x['DictionaryKeyIteratorType'] = type.__dict__.iterkeys()
+x['DictionaryValueIteratorType'] = type.__dict__.itervalues()
+# string services (CH 7)
+# x['InputType'] = cStringIO.InputType #FIXME: write me #XXX: priority
+# x['OutputType'] = cStringIO.OutputType # FIXME: write me #XXX: priority
+x['StructType'] = struct.Struct('c')
+##_callableiter = _srepattern.finditer('')
+##_srematch = _srepattern.match('')
+##_srescanner = _srepattern.scanner('')
+##_streamreader = codecs.StreamReader(_file2) # etc
+# data types (CH 8)
+x['ReferenceType'] = weakref.ref(_instance) #XXX: priority
+# x['DeadReferenceType'] = weakref.ref(_class())
+x['ProxyType'] = weakref.proxy(_instance) #XXX: priority
+# x['DeadProxyType'] = weakref.proxy(_class())
+x['CallableProxyType'] = weakref.proxy(_instance2) #XXX: priority
+# x['DeadCallableProxyType'] = weakref.proxy(_class2())
+x['WeakKeyDictionaryType'] = weakref.WeakKeyDictionary()
+x['WeakValueDictionaryType'] = weakref.WeakValueDictionary()
+# numeric and mathematical types (CH 9)
+x['CycleType'] = itertools.cycle('0')
+x['ItemGetterType'] = operator.itemgetter(0)
+x['AttrGetterType'] = operator.attrgetter('__repr__')
+# python object persistence (CH 11)
+##import shelve; _shelve = shelve.Shelf({})
+##import dbm; _dbm = dbm.open('foo','n')
+##import anydbm; _dbcursor = anydbm.open('foo','n')
+##_db = _dbcursor.db
+# data compression and archiving (CH 12)
+##import zlib; _zcompress = zlib.compressobj()
+##_zdecompress = zlib.decompressobj()
+# file formats (CH 13)
+##_csvreader = csv.reader(_file2)
+##_csvwriter = csv.writer(_file2)
+##_csvdreader = csv.DictReader(_file2)
+##_csvdwriter = csv.DictWriter(_file2,{})
+##import xdrlib; _xdr = xdrlib.Packer()
+# cryptographic services (CH 14)
+##import hashlib; _hash = hashlib.md5()
+##import hmac; _hmac = hmac.new('')
+# generic operating system services (CH 15)
+x['NamedLoggerType'] = logging.getLogger(__name__)
+# interprocess communication (CH 17)
+x['SocketPairType'] = _socket._sock
+
+try: # python 2.6
+    # numeric and mathematical types (CH 9)
+    x['PermutationsType'] = itertools.permutations('0')
+    x['CombinationsType'] = itertools.combinations('0',1)
+    x['MethodCallerType'] = operator.methodcaller('mro') # 2.6
+except AttributeError:
+    pass
+try: # python 2.7
+    ##import locale
+    # built-in types (CH 5)
+    x['DictItemsType'] = _dict.viewitems() # 2.7
+    x['DictKeysType'] = _dict.viewkeys() # 2.7
+    x['DictValuesType'] = _dict.viewvalues() # 2.7
+    x['MemoryType'] = memoryview('0') # 2.7
+    x['MemoryType2'] = memoryview(bytearray('0')) # 2.7
+    # data types (CH 8)
+    x['WeakSetType'] = weakref.WeakSet() # 2.7
+    # numeric and mathematical types (CH 9)
+    x['RepeatType'] = itertools.repeat(0) # 2.7
+    x['CompressType'] = itertools.compress('0',[1]) #XXX: ...and etc
+    ##_cmpkey = functools.cmp_to_key(locale.strcoll) # 2.7
+    ##_cmpkeyobj = _cmpkey('0') #2.7
+except AttributeError:
+    pass
+
+# -- cleanup ----------------------------------------------------------------
+import os; os.remove(_tempfile)
+
+
+# EOF
