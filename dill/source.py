e22303e981fddce77a54f50ac99a3038c23dd4f4
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2014-01-25 00:04:07 +0000

    moved _namespace, likely_import, _likely_import from pox to dill.source; moved _import from klepto.archives to dill.source.getimportable
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/dill@592 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- dill/source.py
+++ dill/source.py
@@ -7,7 +7,8 @@ target of 'dill.source' is to facilitate access to the source
 code of interactively defined functions and classes.
 """
 
-__all__ = ['getblocks_from_history', 'getsource', '_wrap', 'getname']
+__all__ = ['getblocks_from_history', 'getsource', '_wrap', 'getname',\
+           'getimportable, likely_import, _namespace']
 
 import sys
 PYTHON3 = (hex(sys.hexversion) >= '0x30000f0')
@@ -129,11 +130,135 @@ else:
     exec(wrap2)
 del wrap2, wrap3
 
-def getname(obj):
+def getname(obj): #XXX: too simple... pull in logic from getimportable, etc ?
     """ get the name of the object. for lambdas, get the name of the pointer """
     if obj.__name__ == '<lambda>':
         return getsource(obj).split('=',1)[0].strip()
     return obj.__name__
+
+def _namespace(obj):
+    """_namespace(obj); return namespace hierarchy (as a list of names)
+    for the given object.
+
+    For example:
+
+    >>> from functools import partial
+    >>> p = partial(int, base=2)
+    >>> _namespace(p)
+    [\'functools\', \'partial\']
+    """
+    # mostly for functions and modules and such
+    try: #FIXME: this function needs some work and testing on different types
+        from inspect import getmodule, ismodule
+        qual = str(getmodule(obj)).split()[1].strip('"').strip("'")
+        qual = qual.split('.')
+        if ismodule(obj):
+            return qual
+        try: # special case: get the name of a lambda
+            name = getname(obj)
+        except: #XXX: fails to get name
+            name = obj.__name__
+        return qual + [name] #XXX: can be wrong for some aliased objects
+    except: pass
+    # special case: numpy.inf and numpy.nan (we don't want them as floats)
+    if str(obj) in ['inf','nan','Inf','NaN']: # is more, but are they needed?
+        return ['numpy'] + [str(obj)]
+    # mostly for classes and class instances and such
+    module = getattr(obj.__class__, '__module__', None)
+    qual = str(obj.__class__)
+    try: qual = qual[qual.index("'")+1:-2]
+    except ValueError: pass # str(obj.__class__) made the 'try' unnecessary
+    qual = qual.split(".")
+    if module in ['builtins', '__builtin__']:
+        qual = [module] + qual
+    return qual
+
+def _likely_import(first, last, passive=False):
+    """build a likely import string"""
+    # we don't need to import from builtins, so return ''
+    if first in ['builtins','__builtin__']: return ''
+    # get likely import string
+    if not first: _str = "import %s\n" % last
+    else: _str = "from %s import %s\n" % (first, last)
+    # FIXME: breaks on most decorators, currying, and such...
+    #        (could look for magic __wrapped__ or __func__ attr)
+    if not passive:
+       #print(_str)
+        try: exec(_str) #XXX: check if == obj? (name collision)
+        except ImportError: #XXX: better top-down or bottom-up recursion?
+            _first = first.rsplit(".",1)[0] #(or get all, then compare == obj?)
+            if not _first: raise
+            if _first != first:
+                _str = _likely_import(_first, last, passive)
+    return _str
+
+def likely_import(obj, passive=False):
+    """get the likely import string for the given object
+
+    obj: the object to inspect
+    passive: if True, then don't try to verify with an attempted import
+    """
+    # for named things... with a nice repr #XXX: move into _namespace?
+    if not repr(obj).startswith('<'): name = repr(obj).split('(')[0]
+    else: name = None
+    # get the namespace
+    qual = _namespace(obj)
+    first = '.'.join(qual[:-1])
+    last = qual[-1]
+    if name: # try using name instead of last
+        try: return _likely_import(first, name, passive)
+        except (ImportError,SyntaxError): pass
+    try:
+        return _likely_import(first, last, passive)
+    except (ImportError,SyntaxError):
+        raise # could do some checking against obj
+
+
+def getimportable(obj, alias='', byname=True):
+    """attempt to get an importable string that captures the state of obj
+
+For simple objects, this function will discover the name of the object, or the
+repr of the object, or the source code for the object.  To attempt to force
+discovery of the source code, use byname=False.  The intent is to build a
+string that can be imported from a python file.
+    """
+   #try: # get the module name (to see if it's __main__)
+   #    module = str(getmodule(obj)).split()[1].strip('"').strip("'")
+   #except: module = ''
+    try: _import = likely_import(obj)
+    except: _import = ""
+    # try to get the name...
+    memo = None
+    if repr(obj).startswith('<'):
+        try: # get the name (of functions and classes)
+            if byname:
+                _obj = getname(obj)
+            else:
+                _obj = obj.__name__
+                if _obj.startswith('<'): raise # probably a lambda
+                if '__main__' in _import: # we *choose* to use getsource
+                    raise # and don't import from __main__
+            obj = _obj
+        except:
+            try: # try to get the source for lambdas and such
+                memo = getsource(obj, alias=alias)
+            except AttributeError: pass
+        #FIXME: what to do about class instances and such?
+    # hope that it can be built from the __repr__
+    else: obj = repr(obj)
+    # if we got the source, just use it
+    if memo: pass
+    # otherwise, try from __repr__ or __name__
+    elif repr(obj).startswith('<'): #XXX: seems weird for class w byname=False
+        raise AttributeError("object has no atribute '__name__'")
+    elif alias: memo = _import+'%s = %s\n' % (alias,obj)
+    elif _import.endswith('%s\n' % obj): memo = _import
+    else: memo = _import+'%s\n' % obj
+   #print(memo)
+    return memo
+    #XXX: possible failsafe...
+    #     "import dill; memo = dill.loads(<pickled_object>); # repr(<object>)"
+
 
 # backward compatability
 _get_name = getname
--- tests/test_source.py
+++ tests/test_source.py
@@ -1,4 +1,4 @@
-from dill.source import getsource, getname, _wrap
+from dill.source import getsource, getname, _wrap, getimportable, likely_import
 
 f = lambda x: x**2
 def g(x): return f(x) - x
@@ -17,3 +17,87 @@ assert getname(h) == 'h'
 assert _wrap(f)(4) == 16
 assert _wrap(g)(4) == 12
 assert _wrap(h)(4) == 0
+
+
+def add(x,y):
+  return x+y
+
+squared = lambda x:x**2
+
+class Foo(object):
+  def bar(self, x):
+    return x*x+x
+_foo = Foo()
+
+assert getimportable(add) == 'from %s import add\n' % __name__
+assert getimportable(squared) == 'from %s import squared\n' % __name__
+assert getimportable(Foo) == 'from %s import Foo\n' % __name__
+assert getimportable(Foo.bar) == 'from %s import bar\n' % __name__
+assert getimportable(_foo.bar) == 'from %s import bar\n' % __name__
+assert getimportable(None) == 'None\n'
+assert getimportable(100) == '100\n'
+
+assert getimportable(add, byname=False) == 'def add(x,y):\n  return x+y\n'
+assert getimportable(squared, byname=False) == 'squared = lambda x:x**2\n'
+assert getimportable(None, byname=False) == 'None\n'
+assert getimportable(Foo.bar, byname=False) == 'def bar(self, x):\n    return x*x+x\n'
+assert getimportable(Foo.bar, byname=True) == 'from %s import bar\n' % __name__
+assert getimportable(Foo.bar, alias='memo', byname=True) == 'from %s import bar\nmemo = bar\n' % __name__
+#assert getimportable(Foo, byname=False) #FIXME: both f and Foo fail!
+assert getimportable(Foo, alias='memo', byname=True) == 'from %s import Foo\nmemo = Foo\n' % __name__
+assert getimportable(squared, alias='memo', byname=True) == 'from %s import squared\nmemo = squared\n' % __name__
+assert getimportable(squared, alias='memo', byname=False) == 'memo = squared = lambda x:x**2\n'
+assert getimportable(add, alias='memo', byname=False) == 'def add(x,y):\n  return x+y\n\nmemo = add\n'
+assert getimportable(None, alias='memo', byname=False) == 'memo = None\n'
+assert getimportable(100, alias='memo', byname=False) == 'memo = 100\n'
+
+
+try:
+    from numpy import array
+    x = array([1,2,3])
+    assert getimportable(x) == 'from numpy import array\narray([1, 2, 3])\n'
+    assert getimportable(array) == 'from numpy.core.multiarray import array\n'
+    assert getimportable(x, byname=False) == 'from numpy import array\narray([1, 2, 3])\n'
+    assert getimportable(array, byname=False) == 'from numpy.core.multiarray import array\n'
+except ImportError: pass
+
+
+# itself #FIXME: oddly returns 'from dill import likely_import\n'
+#assert likely_import(likely_import)=='from dill.source import likely_import\n'
+
+# builtin functions and objects
+assert likely_import(pow) == ''
+assert likely_import(100) == ''
+assert likely_import(True) == ''
+# this is kinda BS... you can't import a None
+assert likely_import(None) == ''
+
+# other imported functions
+from math import sin
+assert likely_import(sin) == 'from math import sin\n'
+
+# interactively defined functions
+assert likely_import(add) == 'from %s import add\n' % __name__
+
+# interactive lambdas
+assert likely_import(squared) == 'from %s import squared\n' % __name__
+
+# classes and class instances
+try: #XXX: should this be a 'special case'?
+    from StringIO import StringIO
+    x = "from StringIO import StringIO\n"
+    y = x
+except ImportError:
+    from io import BytesIO as StringIO
+    x = "from io import BytesIO\n"
+    y = "from _io import BytesIO\n"
+s = StringIO()
+assert likely_import(StringIO) == x
+assert likely_import(s) == y
+
+# interactively defined classes and class instances
+assert likely_import(Foo) == 'from %s import Foo\n' % __name__
+assert likely_import(_foo) == 'from %s import Foo\n' % __name__
+
+
+# EOF
