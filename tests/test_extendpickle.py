bab6ff0eca8bb9fdfa000f021a1b9809f04d16e0
Bob Fischer <rpf2116@columbia.edu>
2014-02-14 15:42:12 -0500

    Set _main_module when extending StockPickler
    https://github.com/uqfoundation/dill/issues/23

---
+++ tests/test_extendpickle.py
@@ -0,0 +1,20 @@
+import dill as pickle
+import StringIO
+
+def my_fn(x):
+    return x * 17
+
+obj = lambda : my_fn(34)
+assert obj() == 578
+
+obj_io = StringIO.StringIO()
+pickler = pickle.Pickler(obj_io)
+pickler.dump(obj)
+
+obj_str = obj_io.getvalue()
+
+obj2_io = StringIO.StringIO(obj_str)
+unpickler = pickle.Unpickler(obj2_io)
+obj2 = unpickler.load()
+
+assert obj2() == 578
