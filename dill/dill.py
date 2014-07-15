61f8078eae40de61ac5347014a8cd66928c1121e
Matthew Joyce <matsjoyce@gmail.com>
2014-07-15 17:55:13 +0100

    Replace dill function with default funcs for revert_extension

--- dill/dill.py
+++ dill/dill.py
@@ -258,6 +258,8 @@ def dispatch_table():
     return Pickler.dispatch
 '''
 
+pickle_dispatch_copy = StockPickler.dispatch.copy()
+
 def pickle(t, func):
     """expose dispatch table for user-created extensions"""
     Pickler.dispatch[t] = func
@@ -273,6 +275,8 @@ def revert_extension():
     for type, func in list(StockPickler.dispatch.items()):
         if func.__module__ == __name__:
             del StockPickler.dispatch[type]
+            if type in pickle_dispatch_copy:
+                StockPickler.dispatch[type] = pickle_dispatch_copy[type]
 
 def _create_typemap():
     import types
