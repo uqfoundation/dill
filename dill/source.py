f8803f5546b529bdabb93ae5a42d9153092169c6
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2014-07-20 01:54:50 +0000

    importable default (source=None) is 'import if not isfrommain'; added isfrommain and isdynamic;
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/dill@679 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- dill/source.py
+++ dill/source.py
@@ -20,7 +20,7 @@ in a file.
 from __future__ import absolute_import
 __all__ = ['findsource', 'getsourcelines', 'getsource', 'indent', 'outdent', \
            '_wrap', 'dumpsource', 'getname', '_namespace', 'getimport', \
-           '_importable', 'importable']
+           '_importable', 'importable','isdynamic', 'isfrommain']
 
 import re
 import linecache
@@ -29,6 +29,22 @@ from inspect import ismodule, isclass, ismethod, isfunction, istraceback
 from inspect import isframe, iscode, getfile, getmodule, getsourcefile
 from inspect import getblock, indentsize, isbuiltin
 from .dill import PY3
+
+def isfrommain(obj):
+    "check if object was built in __main__"
+    module = getmodule(obj)
+    if module and module.__name__ == '__main__':
+        return True
+    return False
+
+
+def isdynamic(obj):
+    "check if object was built in the interpreter"
+    try: file = getfile(obj)
+    except TypeError: file = None 
+    if file == '<stdin>' and isfrommain(obj):
+        return True
+    return False
 
 
 def _matchlambda(func, line):
@@ -743,16 +759,17 @@ def getimport(obj, alias='', verify=True, builtin=False, enclosing=False):
         raise # could do some checking against obj
 
 
-def _importable(obj, alias='', source=True, enclosing=False, force=True, \
+def _importable(obj, alias='', source=None, enclosing=False, force=True, \
                                               builtin=True, lstrip=True):
     """get an import string (or the source code) for the given object
 
-    For simple objects, this function will discover the name of the object, or
-    the repr of the object, or the source code for the object. To attempt to force
-    discovery of the source code, use source=True, otherwise an import will be
-    sought. The intent is to build a string that can be imported from a python
-    file. obj is the object to inspect. If alias is provided, then rename the
-    object with the given alias.
+    This function will attempt to discover the name of the object, or the repr
+    of the object, or the source code for the object. To attempt to force
+    discovery of the source code, use source=True, to attempt to force the
+    use of an import, use source=False; otherwise an import will be sought
+    for objects not defined in __main__. The intent is to build a string
+    that can be imported from a python file. obj is the object to inspect.
+    If alias is provided, then rename the object with the given alias.
 
     If source=True, use these options:
       If enclosing=True, then also return any enclosing code.
@@ -764,6 +781,8 @@ def _importable(obj, alias='', source=True, enclosing=False, force=True, \
       If force=True, then don't test the import string before returning it.
       If builtin=True, then force an import for builtins where possible.
     """
+    if source is None:
+        source = True if isfrommain(obj) else False
     if source: # first try to get the source
         try:
             return getsource(obj, alias, enclosing=enclosing, \
@@ -881,7 +900,7 @@ def _closuredsource(func, alias=''):
             src = (src, org) # undecorated first, then target
         else: #NOTE: reproduces the code!
             org = getsource(func, enclosing=True, lstrip=False)
-            src = importable(fobj, alias) # include any dependencies
+            src = importable(fobj, alias, source=True) # include dependencies
             src = (org, src) # target first, then decorated
         func_vars[name] = src
     src = ''.join(free_vars.values())
@@ -894,22 +913,28 @@ def _closuredsource(func, alias=''):
     # FIXME: remove duplicates (however, order is important...)
     return func_vars
 
-def importable(obj, alias='', source=True, builtin=True):
+def importable(obj, alias='', source=None, builtin=True):
     """get an importable string (i.e. source code or the import string)
     for the given object, including any required objects from the enclosing
     and global scope
 
     This function will attempt to discover the name of the object, or the repr
     of the object, or the source code for the object. To attempt to force
-    discovery of the source code, use source=True, otherwise an import will be
-    sought. The intent is to build a string that can be imported from a python
-    file. obj is the object to inspect. If alias is provided, then rename the
+    discovery of the source code, use source=True, to attempt to force the
+    use of an import, use source=False; otherwise an import will be sought
+    for objects not defined in __main__. The intent is to build a string
+    that can be imported from a python file.
+
+    obj is the object to inspect. If alias is provided, then rename the
     object with the given alias. If builtin=True, then force an import for
     builtins where possible.
     """
     #NOTE: we always 'force', and 'lstrip' as necessary
     #NOTE: for 'enclosing', use importable(outermost(obj))
-    if builtin and isbuiltin(obj): source = False
+    if source is None:
+        source = True if isfrommain(obj) else False
+    elif builtin and isbuiltin(obj):
+        source = False
     tried_source = tried_import = False
     while True:
         if not source: # we want an import
