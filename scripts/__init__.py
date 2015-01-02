e02316553427890a18258700b7d3ec6fd5ee453e
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2015-01-02 15:37:16 +0000

    updated copyright to 2015
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/dill@728 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,5 +1,5 @@
 # Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
-# Copyright (c) 2008-2014 California Institute of Technology.
+# Copyright (c) 2008-2015 California Institute of Technology.
 # License: 3-clause BSD.  The full license text is available at:
 #  - http://trac.mystic.cacr.caltech.edu/project/pathos/browser/dill/LICENSE
 #
