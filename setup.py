b2792454ee4ace308b900a7c7b6bf2fff8bf68a0
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2015-06-22 12:02:55 +0000

    bumping dill version
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/dill@765 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ import os
 
 # set version numbers
 stable_version = '0.2.3'
-target_version = '0.2.3'
-is_release = True
+target_version = '0.2.4'
+is_release = False
 
 # check if easy_install is available
 try:
