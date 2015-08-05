2694e7033ede202ecc9f6a0339a8ac82a80c220d
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2015-08-05 13:02:39 +0000

    nudge dill past 0.2.4
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/dill@816 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import os
 
 # set version numbers
 stable_version = '0.2.4'
-target_version = '0.2.4'
+target_version = '0.2.5'
 is_release = stable_version == target_version
 
 # check if easy_install is available
