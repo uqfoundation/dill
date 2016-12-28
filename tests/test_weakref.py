54533cdcf8003634d413e1584e3d43b165500cd4
Miguel Sánchez de León Peque <peque@neosit.es>
2016-12-28 23:40:49 +0100

    Make test_weakref.py pytest-friendly

--- tests/test_weakref.py
+++ tests/test_weakref.py
@@ -29,43 +29,49 @@ class _newclass2(object):
 def _function():
     pass
 
-o = _class()
-oc = _class2()
-n = _newclass()
-nc = _newclass2()
-f = _function
-z = _class
-x = _newclass
 
-r = weakref.ref(o)
-dr = weakref.ref(_class())
-p = weakref.proxy(o)
-dp = weakref.proxy(_class())
-c = weakref.proxy(oc)
-dc = weakref.proxy(_class2())
+def test_weakref():
+    o = _class()
+    oc = _class2()
+    n = _newclass()
+    nc = _newclass2()
+    f = _function
+    z = _class
+    x = _newclass
 
-m = weakref.ref(n)
-dm = weakref.ref(_newclass())
-t = weakref.proxy(n)
-dt = weakref.proxy(_newclass())
-d = weakref.proxy(nc)
-dd = weakref.proxy(_newclass2())
+    r = weakref.ref(o)
+    dr = weakref.ref(_class())
+    p = weakref.proxy(o)
+    dp = weakref.proxy(_class())
+    c = weakref.proxy(oc)
+    dc = weakref.proxy(_class2())
 
-fr = weakref.ref(f)
-fp = weakref.proxy(f)
-#zr = weakref.ref(z) #XXX: weakrefs not allowed for classobj objects
-#zp = weakref.proxy(z) #XXX: weakrefs not allowed for classobj objects
-xr = weakref.ref(x)
-xp = weakref.proxy(x)
+    m = weakref.ref(n)
+    dm = weakref.ref(_newclass())
+    t = weakref.proxy(n)
+    dt = weakref.proxy(_newclass())
+    d = weakref.proxy(nc)
+    dd = weakref.proxy(_newclass2())
 
-objlist = [r,dr,m,dm,fr,xr, p,dp,t,dt, c,dc,d,dd, fp,xp]
+    fr = weakref.ref(f)
+    fp = weakref.proxy(f)
+    #zr = weakref.ref(z) #XXX: weakrefs not allowed for classobj objects
+    #zp = weakref.proxy(z) #XXX: weakrefs not allowed for classobj objects
+    xr = weakref.ref(x)
+    xp = weakref.proxy(x)
 
-#dill.detect.trace(True)
-for obj in objlist:
-  res = dill.detect.errors(obj)
-  if res:
-    print ("%s" % res)
-   #print ("%s:\n  %s" % (obj, res))
-# else:
-#   print ("PASS: %s" % obj)
-  assert not res
+    objlist = [r,dr,m,dm,fr,xr, p,dp,t,dt, c,dc,d,dd, fp,xp]
+    #dill.detect.trace(True)
+
+    for obj in objlist:
+      res = dill.detect.errors(obj)
+      if res:
+        print ("%s" % res)
+       #print ("%s:\n  %s" % (obj, res))
+    # else:
+    #   print ("PASS: %s" % obj)
+      assert not res
+
+
+if __name__ == '__main__':
+    test_weakref()
