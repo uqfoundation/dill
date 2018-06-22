b0afa389a6e2076ac172416dd0f5af94d0f9f3ac
mmckerns <mmckerns@8bfda07e-5b16-0410-ab1d-fd04ec2748df>
2018-06-22 22:14:08 +0000

    tag: dill-0.2.8.2
    git-svn-id: svn+ssh://svn.mystic.cacr.caltech.edu/pathos/dill@1081 8bfda07e-5b16-0410-ab1d-fd04ec2748df

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@
 import os
 
 # set version numbers
-stable_version = '0.2.8.1'
-target_version = '0.2.9'
+stable_version = '0.2.8.2'
+target_version = '0.2.8.2'
 is_release = stable_version == target_version
 
 # check if easy_install is available
