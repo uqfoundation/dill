d171ee0ecd65010e6f18ee81a62fdd0c63e3e85a
mmckerns <mmckerns@caltech.edu>
2021-06-13 13:12:22 -0400

    tag dill-0.3.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if unsupported:
     raise ValueError(unsupported)
 
 # set version numbers
-stable_version = '0.3.3'
+stable_version = '0.3.4'
 target_version = '0.3.4'
 is_release = stable_version == target_version
 
