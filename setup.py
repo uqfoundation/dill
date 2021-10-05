107d411f4e744bca12747f5a37dcb0b770bf9962
mmckerns <mmckerns@caltech.edu>
2021-10-05 10:34:48 -0400

    formal support for python 3.10

--- setup.py
+++ setup.py
@@ -345,6 +345,7 @@ setup(name='dill',
                      'Programming Language :: Python :: 3.7',
                      'Programming Language :: Python :: 3.8',
                      'Programming Language :: Python :: 3.9',
+                     'Programming Language :: Python :: 3.10',
                      'Topic :: Scientific/Engineering',
                      'Topic :: Software Development'],
 
