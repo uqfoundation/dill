1094a28f685a330181e6894d0294509b1da08f3c
mmckerns <mmckerns@caltech.edu>
2022-01-01 13:23:39 -0500

    update copyright for 2022

--- tests/test_fglobals.py
+++ tests/test_fglobals.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+#
+# Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
+# Copyright (c) 2021-2022 The Uncertainty Quantification Foundation.
+# License: 3-clause BSD.  The full license text is available at:
+#  - https://github.com/uqfoundation/dill/blob/master/LICENSE
+
 import dill
 dill.settings['recurse'] = True
 
