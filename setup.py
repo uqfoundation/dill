d3d99065869537821bfbf28d50b47f41b08e92d9
mmckerns <mmckerns@caltech.edu>
2022-02-21 18:34:00 -0500

    fixes #451 fixes #452: sunset 3.6

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ import sys
 unsupported = None
 if sys.version_info < (2, 7):
     unsupported = 'Versions of Python before 2.7 are not supported'
-elif (3, 0) <= sys.version_info < (3, 6):
-    unsupported = 'Versions of Python before 3.6 are not supported'
+elif (3, 0) <= sys.version_info < (3, 7):
+    unsupported = 'Versions of Python before 3.7 are not supported'
 if unsupported:
     raise ValueError(unsupported)
 
@@ -170,7 +170,7 @@ Requirements
 
 ``dill`` requires:
 
-    - ``python`` (or ``pypy``), **version == 2.7** or **version >= 3.6**
+    - ``python`` (or ``pypy``), **version == 2.7** or **version >= 3.7**
 
 Optional requirements:
 
@@ -341,7 +341,6 @@ setup(name='dill',
                      'Programming Language :: Python :: 2',
                      'Programming Language :: Python :: 2.7',
                      'Programming Language :: Python :: 3',
-                     'Programming Language :: Python :: 3.6',
                      'Programming Language :: Python :: 3.7',
                      'Programming Language :: Python :: 3.8',
                      'Programming Language :: Python :: 3.9',
