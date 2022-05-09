1885df12fbef56ef7ae09e45486256f5e7a9f56b
Leonardo Gama <leonardo.reis.gama@gmail.com>
2022-05-09 15:42:48 -0300

    Tests: logging code coverage

---
+++ tests/test_logger.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Author: Leonardo Gama (@leogama)
+# Copyright (c) 2022 The Uncertainty Quantification Foundation.
+# License: 3-clause BSD.  The full license text is available at:
+#  - https://github.com/uqfoundation/dill/blob/master/LICENSE
+
+import dill, logging, re
+from dill.logger import formatter, handler, logger
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+def test_logging(regex=None):
+    buffer = StringIO()
+    handler = logging.StreamHandler(buffer)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    try:
+        dill.dumps({'a': (1, 2), 'b': object(), 'big': list(range(10000))})
+        if regex is None:
+            assert buffer.getvalue() == ""
+        else:
+            regex = re.compile(regex + '$')
+            for line in buffer.getvalue().splitlines():
+                assert regex.match(line)
+    finally:
+        logger.removeHandler(handler)
+        buffer.close()
+
+if __name__ == '__main__':
+    logger.removeHandler(handler)
+    test_logging()
+    dill.detect.trace(True)
+    test_logging(r'(# )?\w.*[^)]')
+    dill.detect.trace(True, detail=True)
+    test_logging(r'>+ (# )?\w.* \(\d+ (\wi)?B\)')
+    dill.detect.trace(False)
+    test_logging()
