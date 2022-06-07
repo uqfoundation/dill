960a846a24e9f3348cd785c666b62a0ecf2822cf
anivegesana <anirudh.vegesana@gmail.com>
2022-06-06 21:33:27 -0700

    Update copyright

--- tests/test_pycapsule.py
+++ tests/test_pycapsule.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
 # Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
-# Copyright (c) 2008-2016 California Institute of Technology.
-# Copyright (c) 2016-2022 The Uncertainty Quantification Foundation.
+# Author: Anirudh Vegesana (avegesan@cs.stanford.edu)
+# Copyright (c) 2022 The Uncertainty Quantification Foundation.
 # License: 3-clause BSD.  The full license text is available at:
 #  - https://github.com/uqfoundation/dill/blob/master/LICENSE
 """
