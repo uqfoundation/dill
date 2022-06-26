06970b1e797385411af57062c827138f44879b01
Leonardo Gama <leonardo.reis.gama@gmail.com>
2022-06-26 11:36:01 -0300

    pattern matching: more optimizations

--- dill/_dill.py
+++ dill/_dill.py
@@ -808,9 +808,10 @@ class match:
             return False
         if not all(isinstance(arg, pat) for arg, pat in zip(self.value[len(args):], kwargs.values())):
             return False
-        args = (*args, *kwargs)
-        self.vars = dict(zip(args, self.value))
+        self.args = (*args, *kwargs)
         return True
+    def vars(self):
+        return dict(zip(self.args, self.value))
 
 CODE_PARAMS = [
     # Version     New attribute         CodeType parameters
@@ -826,46 +827,60 @@ for version, new_attr, params in CODE_PARAMS:
         CODE_PARAMS = params.split()
         break
 ENCODE_PARAMS = set(CODE_PARAMS).intersection(
-        ['code', 'lnotab', 'linetable', 'endlinetable', 'columntable', 'exceptiontable'])
+        ['lnotab', 'linetable', 'endlinetable', 'columntable', 'exceptiontable']) # 'code'
+BYTES = (bytes, str)
+BYTES_NONE = (bytes, str, type(None))
 
 def _create_code(*args):
     if not isinstance(args[0], int): # co_lnotab stored from >= 3.10
-        LNOTAB = args[0].encode() if hasattr(args[0], 'encode') else args[0]
-        args = args[1:]
+        LNOTAB, *args = args
     else: # from < 3.10 (or pre-LNOTAB storage)
         LNOTAB = b''
+    if not isinstance(args[5], int): # from 3.7
+        args = args[0], 0, *args[1:] # add posonlyargcount
+    if hasattr(args[6], 'encode'):
+        args[6:7] = (args[6].encode(),) # code
 
-    BYTES = (bytes, str)
-    BYTES_NONE = (bytes, str, type(None))
     with match(args) as m:
-        # Python 3.7
-        if   m.case('argcount', 'kwonlyargcount', 'nlocals', 'stacksize',
-                    'flags', 'code', 'consts', 'names', 'varnames', 'filename', 'name',
-                    firstlineno=int, lnotab=BYTES, freevars=tuple, cellvars=tuple):
-            fields = m.vars
-        # Python 3.9/3.8 or 3.10
-        elif m.case('argcount', 'posonlyargcount', 'kwonlyargcount', 'nlocals', 'stacksize',
+        # Python 3.9/3.8/3.7 or 3.10
+        if   m.case('argcount', 'posonlyargcount', 'kwonlyargcount', 'nlocals', 'stacksize',
                     'flags', 'code', 'consts', 'names', 'varnames', 'filename', 'name',
                     firstlineno=int, LNOTAB_OR_LINETABLE=BYTES, freevars=tuple, cellvars=tuple):
-            fields = m.vars
+            if CODE_VERSION <= (3,10):
+                args = (*args[:13],
+                        args[13].encode() if hasattr(args[13], 'encode') else args[13], # lnotab/linetable
+                        args[14], args[15])
+                if CODE_VERSION == (3,7):
+                    args = args[0], *args[2:] # drop posonlyargcount
+                return CodeType(*args)
+            fields = m.vars()
             key = 'linetable' if CODE_VERSION >= (3,10) else 'lnotab'
-            fields[key] = m.vars['LNOTAB_OR_LINETABLE']
+            fields[key] = fields.pop('LNOTAB_OR_LINETABLE')
         # Python 3.11
         elif m.case('argcount', 'posonlyargcount', 'kwonlyargcount', 'nlocals', 'stacksize',
                     'flags', 'code', 'consts', 'names', 'varnames', 'filename', 'name',
                     qualname=str, firstlineno=int, linetable=BYTES, exceptiontable=BYTES,
                     freevars=tuple, cellvars=tuple):
-            fields = m.vars
+            if CODE_VERSION == (3,11):
+                return CodeType(*args[:14],
+                                args[14].encode() if hasattr(args[14], 'encode') else args[14], # linetable
+                                args[15].encode() if hasattr(args[15], 'encode') else args[15], # exceptiontable
+                                args[16], args[17])
+            fields = m.vars()
         # Python 3.11a
         elif m.case('argcount', 'posonlyargcount', 'kwonlyargcount', 'nlocals', 'stacksize',
                     'flags', 'code', 'consts', 'names', 'varnames', 'filename', 'name',
                     qualname=str, firstlineno=int, linetable=BYTES, endlinetable=BYTES_NONE,
                     columntable=BYTES_NONE, exceptiontable=BYTES, freevars=tuple, cellvars=tuple):
-            fields = m.vars
+            if CODE_VERSION == (3,11,'a'):
+                return CodeType(*args[:14],
+                                *(a.encode() if hasattr(a, 'encode') else a for a in args[14:18]),
+                                args[18], args[19])
+            fields = m.vars()
         else:
             raise UnpicklingError("pattern match for code object failed")
 
-    fields = {k: (v.encode() if k in ENCODE_PARAMS and hasattr(v, 'encode') else v) for k, v in fields.items()}
+    # args format doesn't match this version.
     fields.setdefault('qualname', fields['name'])   # from python <= 3.10
     fields.setdefault('exceptiontable', b'')        # from python <= 3.10
     fields.setdefault('endlinetable', None)         # from python != 3.11a
@@ -877,7 +892,8 @@ def _create_code(*args):
     else:
         fields.setdefault('lnotab', LNOTAB)
 
-    args = (fields[param] for param in CODE_PARAMS)
+    args = (fields[k].encode() if k in ENCODE_PARAMS and hasattr(fields[k], 'encode') else fields[k]
+            for k in CODE_PARAMS)
     return CodeType(*args)
 
 def _create_ftype(ftypeobj, func, args, kwds):
