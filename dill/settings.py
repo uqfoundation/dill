4ad152712c3f5069415a22f3ce2f5ae371b67a49
Leonardo Gama <leonardo.reis.gama@gmail.com>
2022-08-22 14:14:04 -0300

    restore settings.py from master

--- dill/settings.py
+++ dill/settings.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 #
 # Author: Mike McKerns (mmckerns @caltech and @uqfoundation)
-# Author: Leonardo Gama (@leogama)
 # Copyright (c) 2008-2016 California Institute of Technology.
 # Copyright (c) 2016-2022 The Uncertainty Quantification Foundation.
 # License: 3-clause BSD.  The full license text is available at:
@@ -9,8 +8,6 @@
 """
 global settings for Pickler
 """
-
-__all__ = ['settings']
 
 from pickle import DEFAULT_PROTOCOL
 
@@ -25,3 +22,4 @@ settings = {
 }
 
 del DEFAULT_PROTOCOL
+
