a624f4406c36d583db71eb3abe0d07d308f7a37a
Leonardo Gama <leonardo.reis.gama@gmail.com>
2022-08-27 14:01:04 -0300

    tests for module-specific filters and fallbacks

--- dill/tests/test_filtering.py
+++ dill/tests/test_filtering.py
@@ -6,12 +6,13 @@
 #  - https://github.com/uqfoundation/dill/blob/master/LICENSE
 
 import sys
+from types import ModuleType
 
+import dill
 from dill import _dill
-from dill._utils import FilterRules, RuleType, size_filter
-
-EXCLUDE = RuleType.EXCLUDE
-INCLUDE = RuleType.INCLUDE
+from dill.session import (
+    EXCLUDE, INCLUDE, FilterRules, RuleType, ipython_filter, size_filter, settings
+)
 
 NS = {
     'a': 1,
@@ -72,22 +73,74 @@ def test_add_type():
     NS_copy.update(F=test_basic_filtering, T=FilterRules, M=_dill)
     assert did_exclude(NS_copy, type_rules, excluded_subset={'F', 'T', 'M', 'Integer'})
 
-def test_size_filter():
-    from sys import getsizeof
-    estimate = size_filter.estimate_size
+def test_module_filters():
+    R"""Test filters specific for a module and fallback to parent module or default.
 
-    small = list(range(100))
-    large = list(range(1000))
-    reflarge = 10*[small]
-    small_size = getsizeof(small) + 100*getsizeof(0)
-    large_size = getsizeof(large) + 1000*getsizeof(0)
-    assert small_size <= estimate(small) < estimate(reflarge) < large_size <= estimate(large) 
+    The settings['filers'] single-branched tree structure in these tests:
 
-    NS_copy = NS.copy()  # all base objects are small and should not be excluded
-    reflarge.append(reflarge)  # recursive reference
-    NS_copy.update(small=small, large=large, reflarge=reflarge)
-    filter_size = [(EXCLUDE, size_filter(limit=5*small_size))]
-    assert did_exclude(NS_copy, filter_size, excluded_subset={'large'})
+    exclude:      {r'_.*[^_]'}    None            None
+                 /               /               /
+                *-------------* *-------------* *-------------* *~~~~~~~~~~~~~*
+    module:     |   DEFAULT   |-|     foo*    |-|   foo.bar   | | foo.bar.baz |
+                *-------------* *-------------* *-------------* *~~~~~~~~~~~~~*
+                 \               \               \               \_____ _____/
+    include:      {'_keep'}       None            {} (empty)           V
+                                                                    missing
+    (*) 'foo' is a placeholder node
+    """
+    import io
+    foo = sys.modules['foo'] = ModuleType('foo')
+    foo.bar = sys.modules['foo.bar'] = ModuleType('foo.bar')
+    foo.bar.baz = sys.modules['foo.bar.baz'] = ModuleType('foo.bar.baz')
+    NS = {'_filter': 1, '_keep': 2}
+
+    def _dump_load_dict(module):
+        module.__dict__.update(NS)
+        buf = io.BytesIO()
+        dill.dump_module(buf, module)
+        for var in NS:
+            delattr(module, var)
+        buf.seek(0)
+        return dill.load_module_asdict(buf)
+
+    # Empty default filters
+    filters = settings['filters']
+    saved = _dump_load_dict(foo)
+    assert '_filter' in saved
+    assert '_keep' in saved
+
+    # Default filters
+    filters.exclude.add(r'_.*[^_]')
+    filters.include.add('_keep')
+    assert filters.get_rules('foo') is filters
+    saved = _dump_load_dict(foo)
+    assert '_filter' not in saved
+    assert '_keep' in saved
+
+    # Add filters to 'foo.bar' and placeholder node for 'foo'
+    filters['foo.bar'] = ()
+    del filters.foo.bar.exclude # remove empty exclude filters, fall back to default
+    assert not hasattr(filters.foo, 'exclude') and not hasattr(filters.foo, 'include')
+    assert not hasattr(filters.foo.bar, 'exclude') and hasattr(filters.foo.bar, 'include')
+
+    # foo: placeholder node falling back to default
+    assert filters.foo.get_filters(EXCLUDE) is filters.exclude
+    saved = _dump_load_dict(foo)
+    assert '_filter' not in saved
+    assert '_keep' in saved
+
+    # foo.bar: without exclude rules, with (empty) include rules
+    assert filters.foo.bar.get_filters(EXCLUDE) is filters.exclude
+    assert filters.foo.bar.get_filters(INCLUDE) is filters.foo.bar.include
+    saved = _dump_load_dict(foo.bar)
+    assert '_filter' not in saved
+    assert '_keep' not in saved
+
+    # foo.bar.baz: without specific filters, falling back to foo.bar
+    assert filters.get_rules('foo.bar.baz') is filters.foo.bar
+    saved = _dump_load_dict(foo.bar.baz)
+    assert '_filter' not in saved
+    assert '_keep' not in saved
 
 def test_ipython_filter():
     from itertools import filterfalse
@@ -113,9 +166,28 @@ def test_ipython_filter():
     assert namespace_matches(keep_history='both', should_keep_vars={'_i1', '_1'})
     assert namespace_matches(keep_history='none', should_keep_vars=set())
 
+def test_size_filter():
+    from sys import getsizeof
+    estimate = size_filter.estimate_size
+
+    small = list(range(100))
+    large = list(range(1000))
+    reflarge = 10*[small]
+    small_size = getsizeof(small) + 100*getsizeof(0)
+    large_size = getsizeof(large) + 1000*getsizeof(0)
+    assert small_size <= estimate(small) < estimate(reflarge) < large_size <= estimate(large)
+
+    NS_copy = NS.copy()  # all base objects are small and should not be excluded
+    reflarge.append(reflarge)  # recursive reference
+    NS_copy.update(small=small, large=large, reflarge=reflarge)
+    filter_size = [(EXCLUDE, size_filter(limit=5*small_size))]
+    assert did_exclude(NS_copy, filter_size, excluded_subset={'large'})
+
 if __name__ == '__main__':
     test_basic_filtering()
     test_exclude_include()
     test_add_type()
+    test_module_filters()
+    test_ipython_filter()
     if not _dill.IS_PYPY:
         test_size_filter()
--- dill/tests/test_utils.py
+++ dill/tests/test_utils.py
@@ -13,6 +13,14 @@ import sys
 
 from dill import _utils
 
+def test_format_bytes():
+    formatb = _utils._format_bytes_size
+    assert formatb(1000) == (1000, 'B')
+    assert formatb(1024) == (1, 'KiB')
+    assert formatb(1024 + 511) == (1, 'KiB')
+    assert formatb(1024 + 512) == (2, 'KiB')
+    assert formatb(10**9) == (954, 'MiB')
+
 def test_open():
     file_unpeekable = open(__file__, 'rb', buffering=0)
     assert not hasattr(file_unpeekable, 'peek')
@@ -24,9 +32,9 @@ def test_open():
 
     # Test _PeekableReader for seekable stream
     with _utils._open(file_unpeekable, 'r', peekable=True) as file:
+        assert isinstance(file, _utils._PeekableReader)
         assert file.peek(10)[:10] == peeked_chars
         assert file.readline() == first_line
-        assert isinstance(file, _utils._PeekableReader)
     assert not file_unpeekable.closed
     file_unpeekable.close()
 
@@ -39,15 +47,16 @@ def test_open():
 
     # Test io.BufferedReader for unseekable stream
     with _utils._open(pipe_r, 'r', peekable=True) as file:
+        assert isinstance(file, io.BufferedReader)
         pipe_w.write(content[:100])
         assert file.peek(10)[:10] == peeked_chars
         assert file.readline() == first_line
-        assert isinstance(file, io.BufferedReader)
     assert not pipe_r.closed
 
     # Test _SeekableWriter for unseekable stream
     with _utils._open(pipe_w, 'w', seekable=True) as file:
         # pipe_r is closed here for some reason...
+        assert isinstance(file, _utils._SeekableWriter)
         file.write(content)
         file.flush()
         file.seek(0)
@@ -59,14 +68,6 @@ def test_open():
     pipe_r.close()
     pipe_w.close()
 
-def test_format_bytes():
-    formatb = _utils._format_bytes_size
-    assert formatb(1000) == (1000, 'B')
-    assert formatb(1024) == (1, 'KiB')
-    assert formatb(1024 + 511) == (1, 'KiB')
-    assert formatb(1024 + 512) == (2, 'KiB')
-    assert formatb(10**9) == (954, 'MiB')
-
 if __name__ == '__main__':
-    test_open()
     test_format_bytes()
+    test_open()
