55d147115f484e68222c093531aa73135ebfa853
Tomasz Kłoczko <kloczek@github.com>
2024-10-01 09:43:09 +0000

    fix buiding documentation with sphinx 8.x
    Signed-off-by: Tomasz Kłoczko <kloczek@github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -249,7 +249,9 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/3/': None}
+intersphinx_mapping = {
+  'python': ('https://docs.python.org/3', None),
+}
 #    {'python': {'https://docs.python.org/': None},
 #     'mystic': {'https://mystic.readthedocs.io/en/latest/', None},
 #     'pathos': {'https://pathos.readthedocs.io/en/latest/', None},
